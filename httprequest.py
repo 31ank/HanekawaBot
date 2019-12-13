c1a5c5b72075502c862f6d21f1dab12fca482631
David <32846768+31ank@users.noreply.github.com>
2019-12-13 17:38:24 +0100

    Added reddit command

--- httprequest.py
+++ httprequest.py
@@ -9,3 +9,11 @@ def wikipedia(keyword):
         return str(result[2][0])
     else:
         return ""
+
+def redditreq(subreddit, mode = "rising"):
+    r = requests.get("https://www.reddit.com/r/" + subreddit + "/" + mode + ".json?limit=1", headers={'User-agent': 'HanekawaBot'})
+    result = r.json()
+    answer = []
+    answer.append(result['data']['children'][0]['data']['title'])
+    answer.append(result['data']['children'][0]['data']['url'])
+    return answer
--- index.py
+++ index.py
@@ -22,6 +22,25 @@ def wiki(update, context):
     else:
         context.bot.send_message(chat_id = update.effective_chat.id, text = error)
 
+def reddit(update, context):
+    subreddit = ""
+    mode = ""
+    if(context.args[0] != ""):
+        subreddit = context.args[0]
+    try:
+        if(context.args[1] == "rising" or context.args[1] == "hot"):
+            mode = context.args[1]
+    except:
+        mode = "rising"
+    if(subreddit == ""):
+        context.bot.send_message(chat_id = update.effective_chat.id, text = error)
+        return
+    result = req.redditreq(subreddit, mode)
+    if(result[0] != ""):
+        context.bot.send_message(chat_id = update.effective_chat.id, text = result[0] + "\n" + result[1])
+    else:
+        context.bot.send_message(chat_id = update.effective_chat.id, text = error)
+
 def unknown(update, context):
     context.bot.send_message(chat_id = update.effective_chat.id, text = error)
 
@@ -35,6 +54,9 @@ dispatcher.add_handler(start_handler)
 
 wiki_handler = CommandHandler('wiki', wiki)
 dispatcher.add_handler(wiki_handler)
+
+reddit_handler = CommandHandler('r', reddit)
+dispatcher.add_handler(reddit_handler)
 
 unknown_handler = MessageHandler(Filters.command, unknown)
 dispatcher.add_handler(unknown_handler)
